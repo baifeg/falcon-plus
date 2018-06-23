adccc157bf45ee37d260aecc49d0429a3d1ca972
Baifeg <Baifeg@LAPTOP-HOCT7228>
2018-06-23 12:27:05 +0800

    fix some code

--- modules/agent/g/env.go
+++ modules/agent/g/env.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package g
 
 import (
@@ -65,71 +64,3 @@ func InitEnv() {
 		log.Fatalln("read system env failed.", err)
 	}
 }
-=======
-package g
-
-import (
-	"encoding/json"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
-
-const SERVICE_TYPE string = "SPRING_BOOT_WEB"
-
-type SystemEnv struct {
-	Group       string `json:"group"`
-	ServiceType string `json:"serviceType"`
-	Ip          string `json:"ip"`
-	App         string `json:"app"`
-	Tenant      string `json:"tenant"`
-	Service     string `json:"service"`
-	ServicePort int    `json:"servicePort"`
-}
-
-var (
-	env *SystemEnv
-)
-
-func GetEnv() *SystemEnv {
-	return env
-}
-
-func InitEnv() {
-	service := os.Getenv("SERVICE_CODE")
-	group := os.Getenv("SERVICE_DEPLOY_CODE")
-	portKey := strings.ToUpper(group)
-	portKey = strings.Replace(portKey, "-", "_", -1) + "_SERVICE_PORT"
-	servicePort, portOk := os.LookupEnv(portKey)
-	if !portOk {
-		servicePort = "0"
-	}
-	app := os.Getenv("APP_CODE")
-	tenant := os.Getenv("TENANT_CODE")
-	ip := os.Getenv("KETTY_IP")
-	serviceType, ok := os.LookupEnv("SERVICE_TYPE")
-	if !ok {
-		serviceType = SERVICE_TYPE
-	}
-	port, _ := strconv.Atoi(servicePort)
-
-	e := SystemEnv{
-		Group:       group,
-		ServiceType: serviceType,
-		Ip:          ip,
-		App:         app,
-		Tenant:      tenant,
-		Service:     service,
-		ServicePort: port,
-	}
-
-	env = &e
-	j, err := json.Marshal(e)
-	if err == nil {
-		log.Println("read system env done: ", string(j))
-	} else {
-		log.Fatalln("read system env failed.", err)
-	}
-}
->>>>>>> branch 'master' of https://github.com/baifeg/falcon-plus.git
