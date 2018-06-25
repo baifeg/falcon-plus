9f14296e8171060f596f2885c9c23bde29657120
Baifeg <Baifeg@LAPTOP-HOCT7228>
2018-06-25 10:02:02 +0800

    fix hbs

--- modules/hbs/db/group.go
+++ modules/hbs/db/group.go
@@ -51,53 +51,14 @@ func QueryHostGroups() (map[int][]int, error) {
 // 已存在返回false, nil
 // 异常返回false, err
 func PutHostIntoGroupIfNecessary(host, group string) (bool, error) {
-	// 获取group的ID，不存在则创建
-	url := "select id as gid from grp where grp_name=?"
-	rows, err := DB.Query(url, group)
-	if err != nil {
-		log.Println("ERROR:", err)
+	gid, err := getGroupId(group)
+	if err != nil || gid == 0 {
 		return false, err
 	}
 
-	defer rows.Close()
-	var gid int
-	for rows.Next() {
-		rows.Scan(&gid)
-		break
-	}
-	if &gid == nil || gid <= 0 {
-		url = "insert into grp(grp_name, create_user) value (?, ?)"
-		result, err2 := DB.Exec(url, group, "hbs")
-		if err2 != nil {
-			log.Println("ERROR:", err2)
-			return false, err2
-		}
-		if affected, _ := result.RowsAffected(); affected == 0 {
-			log.Printf("ERROR: Add hostgroup[%s] failed\n", group)
-			return false, nil
-		}
-		lastId, _ := result.LastInsertId()
-		gid = int(lastId)
-		log.Printf("Info: Add new hostgroup [%s], id [%d]\n", group, gid)
-	}
-
-	// 获取host的ID，必定存在
-	url = "select id as hid from host where hostname=?"
-	rows2, err3 := DB.Query(url, host)
-	if err3 != nil {
-		log.Println("ERROR:", err3)
-		return false, err3
-	}
-
-	defer rows2.Close()
-	var hid int
-	for rows2.Next() {
-		rows2.Scan(&hid)
-		break
-	}
-
-	if &hid == nil {
-		return false, nil
+	hid, err2 := getHostId
+	if err2 != nil || hid == 0 {
+		return false, err
 	}
 
 	url = "select count(*) as aCount from grp_host where grp_id=? and host_id=?"
@@ -127,4 +88,56 @@ func PutHostIntoGroupIfNecessary(host, group string) (bool, error) {
 	log.Printf("Info: Add host [%s] to group [%s]\n", host, group)
 
 	return true, nil
+}
+
+func getGroupId(group string) (int, error) {
+	// 获取group的ID，不存在则创建
+	url := "select id as gid from grp where grp_name=?"
+	rows, err := DB.Query(url, group)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return 0, err
+	}
+
+	defer rows.Close()
+	var gid int
+	for rows.Next() {
+		rows.Scan(&gid)
+		break
+	}
+	if &gid == nil || gid <= 0 {
+		url = "insert into grp(grp_name, create_user) value (?, ?)"
+		result, err2 := DB.Exec(url, group, "hbs")
+		if err2 != nil {
+			log.Println("ERROR:", err2)
+			return 0, err2
+		}
+		lastId, _ := result.LastInsertId()
+		gid = int(lastId)
+		log.Printf("Info: Add new hostgroup [%s], id [%d]\n", group, gid)
+	}
+	return gid, nil
+}
+
+func getHostId(host string) (int, error) {
+	// 获取host的ID，必定存在
+	url := "select id as hid from host where hostname=?"
+	rows, err := DB.Query(url, host)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return 0, err
+	}
+
+	defer rows.Close()
+	var hid int
+	for rows.Next() {
+		rows.Scan(&hid)
+		break
+	}
+
+	if &hid == nil {
+		return 0, nil
+	}
+	return hid, nil
+
 }
